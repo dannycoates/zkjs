089cbe4c882f7c1d86d3a0b57f8b923a6f2e0c44
Andreas Richter <Andy.Richter@brige-way.com>
2013-04-24 15:52:28 -0400

    Fix join problem under windows.

--- session.js
+++ session.js
@@ -356,7 +356,7 @@ module.exports = function (
 	// internal API
 
 	Session.prototype._chroot = function (path) {
-		path = paths.join(this.root, path)
+		path = paths.join(this.root.replace('/', path.sep), path.replace('/', path.sep)).replace(path.sep, '/');
 		if (path.length > 1 && path[path.length - 1] === '/') {
 			path = path.substring(0, path.length - 1)
 		}
